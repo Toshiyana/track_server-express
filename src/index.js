4ff6bc060e3d011be875d948808cf89329330bec
Toshiyana <yanagimoto-toshiki@dmm.com>
2022-11-03 22:53:50 +0900

    Connecting to MangoDB

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const mongoUri =
+  "mongodb://admin:passwordpassword@ac-0nlk9jj-shard-00-00.tbjjilh.mongodb.net:27017,ac-0nlk9jj-shard-00-01.tbjjilh.mongodb.net:27017,ac-0nlk9jj-shard-00-02.tbjjilh.mongodb.net:27017/?ssl=true&replicaSet=atlas-12q8gw-shard-0&authSource=admin&retryWrites=true&w=majority";
+mongoose.connect(mongoUri);
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to mongo", err);
+});
 
 app.get("/", (req, res) => {
   res.send("Hi there!");
